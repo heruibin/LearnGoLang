c2a768825b86e68188b7e52f6dc15d33539f1943
贺瑞宾 <heruibin@linglongtech.com>
2018-11-19 17:22:38 +0800

    新增类

--- DB/MongoDb/App/MongoUtilsTest.go
+++ DB/MongoDb/App/MongoUtilsTest.go
@@ -1,41 +1,79 @@
 package main
 
 import (
-	"fmt"
-	"gopkg.in/mgo.v2/bson"
-	"time"
-	"Learn/DB/MongoDboDb/MongoByMgoWithPool"
+	"LearnGoLang/DB/MongoDb/MongoByMgoWithPool"
 )
 
 type User struct {
-	Id_       bson.ObjectId `bson:"_id"`
-	Name      string        `bson:"name"`
-	Age       int           `bson:"age"`
-	JoinedAt  time.Time     `bson:"joned_at"`
-	Interests []string      `bson:"interests"`
+	Name string `bson:"name"`
+	Age  int    `bson:"age"`
 }
 
 func main() {
 	MongoByMgoWithPool.Init()
-	session := MongoByMgoWithPool.CloneSession() //调用这个获得session
-	defer session.Close()                        //一定要记得释放
 
-	c := session.DB("music_user").C("people_pool")
-	err := c.Insert(&User{Id_: bson.NewObjectId(),
-		Name: "Jimmy Kuu",
-		Age: 33,
-		JoinedAt: time.Now(),
-		Interests: []string{"Develop", "Movie"}})
+	//var user interface{}
 
-	if err != nil {
-		panic(err)
-	}
+	//user = User{Id_: bson.NewObjectId(),
+	//	Name: "Jimmy Kuu",
+	//	Age: 33,
+	//	JoinedAt: time.Now(),
+	//	Interests: []string{"Develop", "Movie"}}
+	//
+	//MongoByMgoWithPool.Insert("music_user", "test_util", &user)
 
-	var users []User
-	err = c.Find(nil).Limit(5).All(&users)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(users)
+	//var datas map[string]interface{} = make(map[string]interface{})
+	//datas["name"] = "heruibin"
+	//datas["age"] = 28
+	//
+	//MongoByMgoWithPool.Insert("music_user", "test_util2", &datas)
+
+	var dataArr []interface{} = make([]interface{}, 2)
+	var dataArr0 map[string]interface{} = make(map[string]interface{})
+	dataArr0["name"] = "rbhe"
+	dataArr0["age"] = 3
+	dataArr[0] = dataArr0
+	var dataArr1 map[string]interface{} = make(map[string]interface{})
+	dataArr1["name"] = "he"
+	dataArr1["age"] = 4
+	dataArr[1] = dataArr1
+
+	var users []interface{} = make([]interface{}, 2)
+	users[0] = User{Name: "rbhe", Age: 1}
+	users[1] = User{Name: "ruibin", Age: 2}
+
+	MongoByMgoWithPool.InsertBatch("music_user", "test_util3", dataArr)
+
+	//var params map[string]interface{} = make(map[string]interface{})
+	//params["name"] = "heruibin"
+	//
+	//var values map[string]interface{} = make(map[string]interface{})
+	//values["age"] = 30
+	//
+	//var sets map[string]interface{} = make(map[string]interface{})
+	//sets["$set"] = values
+	//
+	//MongoByMgoWithPool.UpdateAll("music_user", "test_util2", params, sets)
+
+	//session := MongoByMgoWithPool.CloneSession() //调用这个获得session
+	//defer session.Close()                        //一定要记得释放
+	//
+	//c := session.DB("music_user").C("people_pool")
+	//err := c.Insert(&User{Id_: bson.NewObjectId(),
+	//	Name: "Jimmy Kuu",
+	//	Age: 33,
+	//	JoinedAt: time.Now(),
+	//	Interests: []string{"Develop", "Movie"}})
+	//
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//var users []User
+	//err = c.Find(nil).Limit(5).All(&users)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//fmt.Println(users)
 
 }
--- DB/MongoDb/MongoByMgoWithPool/MongoUtils.go
+++ DB/MongoDb/MongoByMgoWithPool/MongoUtils.go
@@ -4,24 +4,221 @@ import (
 	"gopkg.in/mgo.v2"
 	"time"
 )
-//global
-var GlobalMgoSession *mgo.Session
 
-func Init() {
-	globalMgoSession, err := mgo.DialWithTimeout("localhost:27017", 10 * time.Second)
+//global
+var MgoSession *mgo.Session
+
+type MongoCfg struct {
+	Addr     string `yaml:"addr"`
+	PoolSize int    `yaml:"poolSize"`
+}
+
+func Init(mongoCfg *MongoCfg) {
+	globalMgoSession, err := mgo.DialWithTimeout(mongoCfg.Addr, 10*time.Second)
 	if err != nil {
 		panic(err)
 	}
-	GlobalMgoSession=globalMgoSession
-	GlobalMgoSession.SetMode(mgo.Monotonic, true)
+	MgoSession = globalMgoSession
+	MgoSession.SetMode(mgo.Monotonic, true)
 	//default is 4096
-	GlobalMgoSession.SetPoolLimit(300)
+	MgoSession.SetPoolLimit(mongoCfg.PoolSize)
 }
 
 func CloneSession() *mgo.Session {
-	return GlobalMgoSession.Clone()
+	return MgoSession.Clone()
 }
 
-func Insert(dbName string,colName string){
+func Insert(dbName string, colName string, data *map[string]interface{}) bool {
+	session := CloneSession()
+	defer session.Close()
 
+	c := session.DB(dbName).C(colName)
+	err := c.Insert(data)
+	if err != nil {
+		panic(err)
+		return false
+	}
+	return true
+}
+
+func InsertBatch(dbName string, colName string, datas []interface{}) bool {
+	session := CloneSession()
+	defer session.Close()
+
+	c := session.DB(dbName).C(colName)
+
+	bulk := c.Bulk()
+	bulk.Unordered()
+	bulk.Insert(datas...)
+	_, err := bulk.Run()
+
+	if err != nil {
+		panic(err)
+		return false
+	}
+	return true
+}
+
+func FindOne(dbName string, colName string, params map[string]interface{}) map[string]interface{} {
+	session := CloneSession()
+	defer session.Close()
+
+	c := session.DB(dbName).C(colName)
+
+	var result map[string]interface{}
+	err := c.Find(params).Limit(1).All(&result)
+	if err != nil {
+		panic(err)
+		return nil
+	}
+	return result
+}
+
+func FindByLimit(dbName string, colName string, params map[string]interface{}, limit int) []map[string]interface{} {
+	session := CloneSession()
+	defer session.Close()
+
+	c := session.DB(dbName).C(colName)
+
+	var results []map[string]interface{}
+	err := c.Find(params).Limit(limit).All(&results)
+	if err != nil {
+		panic(err)
+		return nil
+	}
+	return results
+}
+
+func Update(dbName string, colName string, params map[string]interface{}, values map[string]interface{}) bool {
+	session := CloneSession()
+	defer session.Close()
+
+	c := session.DB(dbName).C(colName)
+
+	err := c.Update(params, values)
+
+	if err != nil {
+		panic(err)
+		return false
+	}
+	return true
+}
+
+func UpdateAll(dbName string, colName string, params map[string]interface{}, values map[string]interface{}) bool {
+	session := CloneSession()
+	defer session.Close()
+
+	c := session.DB(dbName).C(colName)
+
+	_, err := c.UpdateAll(params, values)
+
+	if err != nil {
+		panic(err)
+		return false
+	}
+	return true
+}
+
+func UpSert(dbName string, colName string, params map[string]interface{}, values map[string]interface{}) bool {
+	session := CloneSession()
+	defer session.Close()
+
+	c := session.DB(dbName).C(colName)
+
+	_, err := c.Upsert(params, values)
+
+	if err != nil {
+		panic(err)
+		return false
+	}
+	return true
+}
+
+func DropCol(dbName string, colName string) bool {
+	session := CloneSession()
+	defer session.Close()
+
+	c := session.DB(dbName).C(colName)
+	err := c.DropCollection()
+
+	if err != nil {
+		panic(err)
+		return false
+	}
+	return true
+}
+
+func Remove(dbName string, colName string, params map[string]interface{}) bool {
+	session := CloneSession()
+	defer session.Close()
+
+	c := session.DB(dbName).C(colName)
+	err := c.Remove(params)
+
+	if err != nil {
+		panic(err)
+		return false
+	}
+	return true
+}
+
+func RemoveAll(dbName string, colName string, params map[string]interface{}) bool {
+	session := CloneSession()
+	defer session.Close()
+
+	c := session.DB(dbName).C(colName)
+	_, err := c.RemoveAll(params)
+
+	if err != nil {
+		panic(err)
+		return false
+	}
+	return true
+}
+
+func RemoveBatch(dbName string, colName string, params []interface{}) bool {
+	session := CloneSession()
+	defer session.Close()
+
+	c := session.DB(dbName).C(colName)
+
+	bulk := c.Bulk()
+	bulk.Remove(params...)
+	_, err := bulk.Run()
+
+	if err != nil {
+		panic(err)
+		return false
+	}
+	return true
+}
+
+func RemovesBatchAll(dbName string, colName string, params []interface{}) bool {
+	session := CloneSession()
+	defer session.Close()
+
+	c := session.DB(dbName).C(colName)
+	bulk := c.Bulk()
+	bulk.Remove(params...)
+	_, err := bulk.Run()
+
+	if err != nil {
+		panic(err)
+		return false
+	}
+	return true
+}
+
+func Count(dbName string, colName string, params map[string]interface{}) int {
+	session := CloneSession()
+	defer session.Close()
+
+	c := session.DB(dbName).C(colName)
+	count, err := c.Find(params).Count()
+
+	if err != nil {
+		panic(err)
+		return -1
+	}
+	return count
 }
